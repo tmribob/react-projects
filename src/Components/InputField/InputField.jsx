888a160e4c3a5fa5102670ae203589c2f9f4b30a
Tmribob <mmitko2141@gmail.com>
2025-04-26 14:57:06 +0300

    add in InputField.jsx attribute readonly and css style :focus

--- src/Components/InputField/InputField.jsx
+++ src/Components/InputField/InputField.jsx
@@ -1,11 +1,12 @@
 import style from "./InputField.module.css"
 
-const InputField = ({name,value, holder, onChange}) => {
+const InputField = ({name,value, holder, onChange,readonly=null}) => {
     return (
         <input name={name}
                value={value}
                placeholder={holder}
                onChange={onChange}
+               readOnly={readonly}
                className={style.inputText}/>
     )
 }
