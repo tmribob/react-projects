a76f58876b56ab209dac80830d8480d5d3c4daaa
Tmribob <mmitko2141@gmail.com>
2025-05-01 17:13:47 +0300

    fix css style Notification.jsx and CalculatorField.module.css

--- src/Components/Notification/Notification.jsx
+++ src/Components/Notification/Notification.jsx
@@ -1,9 +1,9 @@
 import style from "./Notification.module.css"
 
-const Notification = ({context}) => {
+const Notification = ({context, isVisible}) => {
 
-    return(<div className={style.notification}>
-        <p>{context}</p>
+    return (<div className={`${style.notification} ${isVisible && style.visibility}`}>
+        <p className={style.text}>{context}</p>
     </div>)
 }
 
