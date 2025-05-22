5a8bccca0e90a3d274864328161e24d2a178f056
Tmribob <mmitko2141@gmail.com>
2025-05-22 22:02:31 +0300

    add width and height for Input.jsx

--- src/Components/Input/Input.jsx
+++ src/Components/Input/Input.jsx
@@ -1,13 +1,25 @@
 import style from "./Input.module.css"
 
-const Input = ({name, value, holder = null, onChange, disabled = false}) => {
-    return (
-        <input name={name}
-               value={value}
-               placeholder={holder}
-               onChange={onChange}
-               disabled={disabled}
-               className={style.inputText}/>
+const Input = ({
+                   name,
+                   value,
+                   holder = null,
+                   onChange,
+                   disabled = false,
+                   width = "100%",
+                   height = "100%",
+                   position = "left"
+               }) => {
+    return (<div className={style.divInput} style={{width: width, height: height}}>
+            <input name={name}
+                   value={value}
+                   placeholder={holder}
+                   onChange={onChange}
+                   disabled={disabled}
+                   style={{textAlign: position}}
+                   className={style.inputText}/>
+        </div>
+
     )
 }
 
